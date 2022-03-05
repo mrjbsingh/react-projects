25dc4babb0f0e1bc87c9017907f76a8558fb2472
mrjbsingh <jayendra.iiita@gmail.com>
2022-03-05 10:55:53 +0530

    getting todo data from api and loaded it in formik

--- src/api/todo/TodoDataService.js
+++ src/api/todo/TodoDataService.js
@@ -8,6 +8,15 @@ class TodoDataService{
         return axios.get('http://localhost:8080/users/'+username+'/todos');
  
     }
+
+    executeTodoById(username, id){
+        console.log("Todo by id "+ id);
+        console.log('http://localhost:8080/users/'+username+'/todo/'+id);
+        //console.log(axios.get('http://localhost:8080/users/'+username+'/todos'));
+        return axios.get('http://localhost:8080/users/'+username+'/todo/'+id);
+ 
+    }
+    
     deleteTodo(username, id){
         console.log("Service call for deleting Todo "+ username);
         console.log('http://localhost:8080/users/'+username+'/todos/'+id);
--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -1,7 +1,9 @@
-import { Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import moment from "moment";
 import react, { Component } from "react";
 import { useHistory, useParams } from "react-router-dom";
+import TodoDataService from "../../api/todo/TodoDataService";
+import AuthenticationService from "./AuthenticationService";
 
 class TodoComponent extends Component {
   constructor(props) {
@@ -9,31 +11,95 @@ class TodoComponent extends Component {
 
     this.state = {
       id: this.props.params.id,
-      description: "Learn Java",
+      description: "",
       targetDate: moment(new Date()).format("YYYY-MM-DD"),
     };
   }
+
+  validates(values) {
+    let error = {}; //please don't forget to define object else error msg will not be shown
+    if (!values.description) {
+      error.description = "Please Enter a Description";
+    } else if (values.description.length < 5) {
+      error.description = "Please Enter atleast 5 character in Description";
+    }
+
+    if (!moment(values.targetDate).isValid()) {
+      error.targetDate = "Enter a valid Date";
+    }
+    console.log(error);
+    return error;
+  }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
+  componentDidMount() {
+    let username = AuthenticationService.getLoggedInUser();
+    this.loadTodoById(username, this.state.id);
+  }
+  loadTodoById(username, id) {
+    TodoDataService.executeTodoById(username, id).then((response) => {
+      console.log("response from todo by id");
+      console.log(response.data);
+      this.setState({
+        description: response.data.description,
+        targetDate: moment(response.data.targetDate).format("YYYY-MM-DD"),
+      });
+    });
+  }
+
   render() {
+    let description = this.state.description;
+    let targetDate = this.state.targetDate;
     return (
-      <> <h1>Edit Todo</h1>
+      <>
+        {" "}
+        <h1>Edit Todo</h1>
         <div className="container">
-            <Formik>
-                {
-                    (props) => (
-                        <Form>
-                            <fieldset className="form-group">
-                                <label>Description</label>
-                                <Field className="form-control" type="text" name="description"></Field>
-                            </fieldset>
-                            <fieldset className="form-group">
-                                <label>Target Date</label>
-                                <Field className="form-control" type="date" name="targetDate"></Field>
-                            </fieldset>
-                            <button className="btn btn-success">Save</button>
-                        </Form>
-                    )
-                }
-            </Formik>
+          <Formik
+            initialValues={{ description, targetDate }}
+            onSubmit={this.onSubmit}
+            validateOnBlur={false}
+            validateOnChange={false}
+            validate={this.validates}
+            enableReinitialize={true}
+          >
+            {(props) => (
+              <Form>
+                <ErrorMessage
+                  name="description"
+                  component="div"
+                  className="alert alert-warning"
+                ></ErrorMessage>
+                <ErrorMessage
+                  name="targetDate"
+                  component="div"
+                  className="alert alert-warning"
+                ></ErrorMessage>
+                <fieldset className="form-group">
+                  <label>Description</label>
+                  <Field
+                    className="form-control"
+                    type="text"
+                    name="description"
+                  ></Field>
+                </fieldset>
+                <fieldset className="form-group">
+                  <label>Target Date</label>
+                  <Field
+                    className="form-control"
+                    type="date"
+                    name="targetDate"
+                  ></Field>
+                </fieldset>
+                <button className="btn btn-success" type="submit">
+                  Save
+                </button>
+              </Form>
+            )}
+          </Formik>
         </div>
         <div>Todo Component for id - {this.state.id}</div>
       </>
